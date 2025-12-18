5277ed96f39f8fd6d8874cbbfb45e1efcd76b296
phanirithvij <phanirithvij2000@gmail.com>
2025-12-18 13:55:11 +0530

    WIP types in manage.py
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- src/goupile/dist/linux/manage.py
+++ src/goupile/dist/linux/manage.py
@@ -11,42 +11,47 @@ import subprocess
 import sys
 from dataclasses import dataclass
 
-GOUPILE_BINARY = '/usr/bin/goupile'
+GOUPILE_BINARY = "/usr/bin/goupile"
 
-DOMAINS_DIRECTORY = '/etc/goupile/domains.d'
-TEMPLATE_FILENAME = '/etc/goupile/template.ini'
+DOMAINS_DIRECTORY = "/etc/goupile/domains.d"
+TEMPLATE_FILENAME = "/etc/goupile/template.ini"
 
-CONFIG_DIRECTORY = '/run/goupile'
-ROOT_DIRECTORY = '/var/lib/goupile'
+CONFIG_DIRECTORY = "/run/goupile"
+ROOT_DIRECTORY = "/var/lib/goupile"
 
-GENERATOR_DIRECTORY = '/run/systemd/generator'
-UNIT_FILENAME = '/usr/lib/systemd/system/goupile@.service'
+GENERATOR_DIRECTORY = "/run/systemd/generator"
+UNIT_FILENAME = "/usr/lib/systemd/system/goupile@.service"
 
 FIRST_PORT = 8889
 
+
 @dataclass
 class DomainConfig:
-    name = None
-    port = None
-    demo = False
-    mtime = None
+    name: str = ""
+    port: str = ""
+    demo: bool = False
+    mtime: int = -1
+
 
 @dataclass
 class ServiceStatus:
-    running = False
-    inode = None
-    since = None
+    running: bool = False
+    inode: int = -1
+    since: int = -1
+
 
 def create(args):
-    if not re.match(r'^[a-zA-Z0-9_\-\.]+$', args.name):
-        raise ValueError('Name must only contain alphanumeric, \'.\', \'-\' or \'_\' characters')
+    if not re.match(r"^[a-zA-Z0-9_\-\.]+$", args.name):
+        raise ValueError(
+            "Name must only contain alphanumeric, '.', '-' or '_' characters"
+        )
 
     domains = load_domains(DOMAINS_DIRECTORY)
-    ports = { domain.port for domain in domains }
+    ports = {domain.port for domain in domains}
 
-    if args.port is not None:
+    if args.port is not "":
         if args.port in ports:
-            raise ValueError(f'Port {args.port} is already in use')
+            raise ValueError(f"Port {args.port} is already in use")
 
         port = args.port
     else:
@@ -55,23 +60,20 @@ def create(args):
             port += 1
         port = str(port)
 
-    ini_filename = os.path.join(DOMAINS_DIRECTORY, args.name) + '.ini'
+    ini_filename = os.path.join(DOMAINS_DIRECTORY, args.name) + ".ini"
     config = make_domain_config(port)
 
     if os.path.exists(ini_filename) and not args.force:
-        raise ValueError(f'Domain {args.name} already exists');
-    with open(ini_filename, 'w' if args.force else 'x') as f:
+        raise ValueError(f"Domain {args.name} already exists")
+    with open(ini_filename, "w" if args.force else "x") as f:
         config.write(f)
 
-    print(f'Domain config file: {ini_filename}', file = sys.stderr)
-    print(f'HTTP port: {port}', file = sys.stderr)
+    print(f"Domain config file: {ini_filename}", file=sys.stderr)
+    print(f"HTTP port: {port}", file=sys.stderr)
 
     if args.sync:
-        domains = load_domains(DOMAINS_DIRECTORY)
+        sync(args)
 
-        sync_domains(CONFIG_DIRECTORY, domains, TEMPLATE_FILENAME)
-        sync_units(GENERATOR_DIRECTORY, domains)
-        sync_services(domains)
 
 def sync(args):
     domains = load_domains(DOMAINS_DIRECTORY)
@@ -80,42 +82,44 @@ def sync(args):
     sync_units(GENERATOR_DIRECTORY, domains)
     sync_services(domains)
 
-def load_config(filename):
+
+def load_config(filename: str):
     config = configparser.ConfigParser()
     config.optionxform = str
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         config.read_file(f)
 
     return config
 
-def load_domains(path):
-    domains = []
 
-    used_names = set()
-    used_ports = set()
+def load_domains(path: str) -> list[DomainConfig]:
+    domains: list[DomainConfig] = []
+
+    used_names: set[str] = set()
+    used_ports: set[str] = set()
 
     for name in os.listdir(path):
-        if not name.endswith('.ini'):
+        if not name.endswith(".ini"):
             continue
 
         filename = os.path.join(path, name)
         name, _ = os.path.splitext(name)
 
         config = load_config(filename)
-        section = config['Domain']
+        section = config["Domain"]
 
         domain = DomainConfig()
 
         domain.name = name
-        domain.port = decode_port(section['Port'])
-        if 'DemoMode' in section:
-            domain.demo = decode_bool(section['DemoMode'])
+        domain.port = decode_port(section["Port"])
+        if "DemoMode" in section:
+            domain.demo = decode_bool(section["DemoMode"])
 
         if domain.name in used_names:
             raise ValueError(f'Name "{domain.name}" used multiple times')
         if domain.port in used_ports:
-            raise ValueError(f'Port {domain.port} used multiple times')
+            raise ValueError(f"Port {domain.port} used multiple times")
 
         used_names.add(domain.name)
         used_ports.add(domain.port)
@@ -124,68 +128,72 @@ def load_domains(path):
 
     return domains
 
-def decode_port(port):
+
+def decode_port(port: str):
     if port.isnumeric():
         if int(port) <= 0 or int(port) > 65535:
             raise ValueError(f'Invalid TCP port "{port}"')
     else:
-        if not port.startswith('/'):
-            raise ValueError(f'Use absolute path for UNIX socket')
+        if not port.startswith("/"):
+            raise ValueError(f"Use absolute path for UNIX socket")
     return port
 
-def decode_bool(s):
+
+def decode_bool(s: str) -> bool:
     s = s.lower()
 
-    if s in ('y', 'yes', 't', 'true', 'on', '1'):
+    if s in ("y", "yes", "t", "true", "on", "1"):
         return True
-    elif s in ('n', 'no', 'f', 'false', 'off', '0'):
+    elif s in ("n", "no", "f", "false", "off", "0"):
         return False
     else:
         raise ValueError(f'Invalid truth value "{s}"')
 
-def sync_domains(path, domains, template):
-    os.makedirs(path, mode = 0o755, exist_ok = True)
+
+def sync_domains(path: str, domains: list[DomainConfig], template: str):
+    os.makedirs(path, mode=0o755, exist_ok=True)
 
     for domain in domains:
         config = load_config(template)
 
-        root_directory = os.path.join(ROOT_DIRECTORY, domain.name)
-        ini_filename = os.path.join(path, domain.name) + '.ini'
+        root_directory: str = os.path.join(ROOT_DIRECTORY, domain.name)
+        ini_filename: str = os.path.join(path, domain.name) + ".ini"
 
-        data = list(config['Data'].items())
+        data = list(config["Data"].items())
 
-        config['Data']['RootDirectory'] = root_directory
-        config['Data']['UseSnapshots'] = 'Off' if domain.demo else 'On'
+        config["Data"]["RootDirectory"] = root_directory
+        config["Data"]["UseSnapshots"] = "Off" if domain.demo else "On"
         for key, value in data:
-            if not key in config['Data']:
-                config['Data'][key] = value
-        config['Demo']['DemoMode'] = 'On' if domain.demo else 'Off'
+            if not key in config["Data"]:
+                config["Data"][key] = value
+        config["Demo"]["DemoMode"] = "On" if domain.demo else "Off"
         if domain.port.isnumeric():
-            config['HTTP']['SocketType'] = 'Dual'
-            config['HTTP']['Port'] = domain.port
+            config["HTTP"]["SocketType"] = "Dual"
+            config["HTTP"]["Port"] = domain.port
         else:
-            config['HTTP']['SocketType'] = 'Unix'
-            config['HTTP']['UnixPath'] = domain.port
+            config["HTTP"]["SocketType"] = "Unix"
+            config["HTTP"]["UnixPath"] = domain.port
 
         with io.StringIO() as f:
             config.write(f)
             buf = f.getvalue()
         if os.path.exists(ini_filename):
-            with open(ini_filename, 'r') as f:
+            with open(ini_filename, "r") as f:
                 prev = f.read()
         else:
             prev = None
         if buf != prev:
-            with open(ini_filename, 'w') as f:
-                f.write(buf)
+            with open(ini_filename, "w") as f:
+                _ = f.write(buf)
 
         domain.mtime = int(os.path.getmtime(ini_filename))
 
-def sync_units(path, domains):
-    directory = os.path.join(path, 'multi-user.target.wants')
+
+def sync_units(path: str, domains: list[DomainConfig]):
+    directory = os.path.join(path, "multi-user.target.wants")
     prefix, _ = os.path.splitext(os.path.basename(UNIT_FILENAME))
 
-    os.makedirs(directory, exist_ok = True)
+    os.makedirs(directory, exist_ok=True)
 
     for name in os.listdir(directory):
         if not name.startswith(prefix):
@@ -195,35 +203,64 @@ def sync_units(path, domains):
         os.unlink(unit_filename)
 
     for domain in domains:
-        unit_filename = os.path.join(directory, prefix + domain.name) + '.service'
+        unit_filename = os.path.join(directory, prefix + domain.name) + ".service"
         os.symlink(UNIT_FILENAME, unit_filename)
 
-def sync_services(domains):
-    domains = { domain.name: domain for domain in domains }
-    services = {}
 
-    output = subprocess.check_output(['systemctl', 'list-units', '--type=service', '--all'])
+def sync_services(domains_list: list[DomainConfig]):
+    domains: dict[str, DomainConfig] = {domain.name: domain for domain in domains_list}
+    services: dict[str, ServiceStatus] = {}
+
+    output = subprocess.check_output(
+        ["systemctl", "list-units", "--type=service", "--all"]
+    )
     output = output.decode()
 
     for line in output.splitlines():
-        parts = re.split(' +', line)
+        parts = re.split(" +", line)
 
         if len(parts) >= 4:
-            match = re.search('^goupile@([0-9A-Za-z_\\-\\.]+)\\.service$', parts[1])
+            match = re.search("^goupile@([0-9A-Za-z_\\-\\.]+)\\.service$", parts[1])
 
             if match is not None:
                 name = match.group(1)
 
                 status = ServiceStatus()
-                status.running = (parts[3] == 'active')
+                status.running = parts[3] == "active"
 
                 if status.running:
                     try:
-                        pid = int(subprocess.check_output(['systemctl', 'show', '-p', 'ExecMainPID', '--value', parts[1]]))
-                        sb = os.stat(f'/proc/{pid}/exe')
+                        pid = int(
+                            subprocess.check_output(
+                                [
+                                    "systemctl",
+                                    "show",
+                                    "-p",
+                                    "ExecMainPID",
+                                    "--value",
+                                    parts[1],
+                                ]
+                            )
+                        )
+                        sb = os.stat(f"/proc/{pid}/exe")
 
-                        start = subprocess.check_output(['systemctl', 'show', '-p', 'ExecMainStartTimestamp', '--value', parts[1]]).decode('utf-8').strip()
-                        since = int(subprocess.check_output(['date', '--date=' + start, '+%s']))
+                        start = (
+                            subprocess.check_output(
+                                [
+                                    "systemctl",
+                                    "show",
+                                    "-p",
+                                    "ExecMainStartTimestamp",
+                                    "--value",
+                                    parts[1],
+                                ]
+                            )
+                            .decode("utf-8")
+                            .strip()
+                        )
+                        since = int(
+                            subprocess.check_output(["date", "--date=" + start, "+%s"])
+                        )
 
                         status.inode = sb.st_ino
                         status.since = since
@@ -238,52 +275,74 @@ def sync_services(domains):
         domain = domains.get(name)
 
         if domain is None:
-            run_service_command(name, 'stop')
+            run_service_command(name, "stop")
 
     for domain in domains.values():
         status = services.get(domain.name)
 
         if status is None:
-            run_service_command(domain.name, 'restart')
+            run_service_command(domain.name, "restart")
         elif not status.running:
-            run_service_command(domain.name, 'restart')
+            run_service_command(domain.name, "restart")
         elif status.inode != binary_inode:
-            run_service_command(domain.name, 'restart')
-        elif domain.mtime is not None and status.since < domain.mtime:
-            run_service_command(domain.name, 'restart')
+            run_service_command(domain.name, "restart")
+        elif domain.mtime != -1 and status.since < domain.mtime:
+            run_service_command(domain.name, "restart")
 
-def run_service_command(name, cmd):
-    service = f'goupile@{name}.service'
 
-    print(f'{cmd.capitalize()} {service}')
-    execute_command(['systemctl', cmd, '--quiet', service])
+def run_service_command(name: str, cmd: str):
+    service = f"goupile@{name}.service"
 
-def make_domain_config(port):
+    print(f"{cmd.capitalize()} {service}")
+    execute_command(["systemctl", cmd, "--quiet", service])
+
+
+def make_domain_config(port: str):
     config = configparser.ConfigParser()
     config.optionxform = str
 
-    config.add_section('Domain')
-    config['Domain']['Port'] = port
-    config['Domain']['DemoMode'] = 'Off'
+    config.add_section("Domain")
+    config["Domain"]["Port"] = port
+    config["Domain"]["DemoMode"] = "Off"
 
     return config
 
+
 def execute_command(args):
-    subprocess.run(args, check = True)
+    _ = subprocess.run(args, check=True)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Manage goupile services')
-    subparsers = parser.add_subparsers(required = True)
 
-    sub = subparsers.add_parser('create')
-    sub.add_argument('name', metavar = 'name', type = str, help = 'domain name')
-    sub.add_argument('-f', '--force', dest = 'force', action = 'store_true', help = 'Overwrite existing domain config')
-    sub.add_argument('-p', '--port', dest = 'port', type = decode_port, required = False, help = 'HTTP port or unix socket')
-    sub.add_argument('--no_sync', dest = 'sync', action = 'store_false', help = 'Skip automatic domain sync')
-    sub.set_defaults(func = create)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Manage goupile services")
+    subparsers = parser.add_subparsers(required=True)
 
-    sub = subparsers.add_parser('sync', help = 'Sync goupile services')
-    sub.set_defaults(func = sync)
+    sub = subparsers.add_parser("create")
+    _ = sub.add_argument("name", metavar="name", type=str, help="domain name")
+    _ = sub.add_argument(
+        "-f",
+        "--force",
+        dest="force",
+        action="store_true",
+        help="Overwrite existing domain config",
+    )
+    _ = sub.add_argument(
+        "-p",
+        "--port",
+        dest="port",
+        type=decode_port,
+        required=False,
+        help="HTTP port or unix socket",
+    )
+    _ = sub.add_argument(
+        "--no_sync",
+        dest="sync",
+        action="store_false",
+        help="Skip automatic domain sync",
+    )
+    sub.set_defaults(func=create)
+
+    sub = subparsers.add_parser("sync", help="Sync goupile services")
+    sub.set_defaults(func=sync)
 
     args = parser.parse_args()
     args.func(args)
